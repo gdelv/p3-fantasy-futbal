b7f129d176dd681b5085d220a8b1fd8687493442
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2019-12-09 00:55:03 -0500

    added Home/Info/Players/Roster/Signupin components

--- fntsyfutbal-app/src/App.js
+++ fntsyfutbal-app/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Route, withRouter } from 'react-router-dom'
 
-function App() {
-  return (
+import Home from './components/routes/Home'
+import SignUpIn from './components/routes/SignUpIn'
+import Info from './components/routes/Info'
+import Roster from './components/routes/Roster'
+import Players from './components/routes/Players'
+
+const App = (props) => (
+  <React.Fragment>
     <div className="App">
       <h1>hello</h1>
+      {/* <h3>{props.location.state ? props.location.state.msg : null}</h3> */}
     </div>
-  );
-}
+  </React.Fragment>  
+    
+
+)
 
 export default App;
