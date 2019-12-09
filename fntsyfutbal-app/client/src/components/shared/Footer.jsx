652f32bf017d26a30c8b1019f88285062e42bb88
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2019-12-09 13:48:21 -0500

    fixed front end to run correctly

---
+++ fntsyfutbal-app/client/src/components/shared/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Footer = () => (
+    <React.Fragment>
+        <p>© Copyright {new Date().getFullYear()}. All Rights Reserved.</p>
+    <p>Created By:</p>
+    <p>Jeanmax Deslouches, Frenchy French, Giusseppe Del Vecchio</p>
+    </React.Fragment>
+    
+)
+
+export default Footer
---
+++ fntsyfutbal-app/client/src/components/shared/Input.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './styles/Input.css'
+
+const Input = ({ onChange, name, placeholder, required, value }) => {
+    <div className='input'>
+        <input
+            value={value}
+            name={name}
+            required={required}
+            onChange={e => onChange(e)}
+        />
+        <label htmlFor={name}>
+            <span>{ placeholder }</span>
+        </label>
+      
+
+    </div>
+}
+
+export default Input
