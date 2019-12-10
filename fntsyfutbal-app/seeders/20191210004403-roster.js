2936951355b9ccafed3f0709ca948c4b3026bf89
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2019-12-10 13:53:59 -0500

    fixed seeders file

--- fntsyfutbal-app/seeders/20191210004403-roster.js
+++ fntsyfutbal-app/seeders/20191210004403-roster.js
@@ -3,8 +3,8 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Rosters', [{
-      user_id: 2,
-      player_id: 1,
+      // user_id: 2,
+      // player_id: 1,
       title: 'My First Roster',
       createdAt: new Date(),
       updatedAt: new Date()
