9a38643a42c9f275f910f613769bc80e111eb6f3
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2019-12-11 17:14:16 -0500

    create account working good

--- fntsyfutbal-app/client(front-end)/src/screens/signedOut/Join.jsx
+++ fntsyfutbal-app/client(front-end)/src/screens/signedOut/Join.jsx
@@ -1,57 +1,171 @@
-import React from 'react'
-import Layout from '../../components/shared/Layout'
-import CreateAccForm from '../forms/CreateAccForm'
-import { api } from '../../services/apiConfig'
+// import React from 'react'
+// import Layout from '../../components/shared/Layout'
+// import CreateAccForm from '../forms/CreateAccForm'
+// import { api } from '../../services/apiConfig'
 
-class Join extends React.Component {
-    constructor(props) {
-        super(props)
+// class Join extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             username: '',
+//             password: '',
+//             firstName: '',
+//             lastName: '',
+//             email: '',
+//             imgUrl: '',
+//             errorMsg: ''
+//         }
+//     }
+
+//     handleSubmit = (event) => {
+//         event.preventDefault()
+//         const { username, firstName, lastName, email, password, imgUrl } = this.state
+//         const data = {
+//             username,
+//             password,
+//             firstName,
+//             lastName,
+//             email,
+//             imgUrl
+//         }
+
+//         // console.log({password})
+//         // console.log(data)
+//         api.post('/users', data)
+//             .then((response) => response.status === 201 ? this.props.history.push('/') : null)
+//             .catch(() => this.setState({ errorMsg: 'There was an error' }))
+//     }
+
+//     handleChange = e => this.setState({ [e.target.name]: e.target.value })
+
+//     render() {
+//         const { username, firstName, lastName, email, password, imgUrl } = this.state
+//         return (
+//             <Layout>
+//                 <h4>Create your account!</h4>
+//                 <CreateAccForm
+//                     formData={{ username, firstName, lastName, email, password, imgUrl }}
+//                     onChange={this.handleChange}
+//                     onSubmit={this.handleSubmit}
+//                 />
+//             </Layout>
+//         )
+//     }
+// }
+
+
+// export default Join
+
+
+import React, { Component } from 'react'
+import { join, logIn } from '../../services/auth'
+
+class SignUp extends Component {
+    constructor() {
+        super()
+
         this.state = {
             username: '',
-            password: '',
-            firstName: '',
-            lastName: '',
             email: '',
-            imgUrl: '',
+            password: '',
+            passwordConfirmation: '',
+            isError: false,
             errorMsg: ''
         }
     }
 
-    handleSubmit = (event) => {
+    handleChange = event => 
+        this.setState({
+            [event.target.name]: event.target.value,
+            isError: false,
+            errorMsg: ''
+        })
+
+    onSignUp =  event  => {
         event.preventDefault()
-        const { username, firstName, lastName, email, password, imgUrl } = this.state
-        const data = {
-            username,
-            password,
-            firstName,
-            lastName,
-            email,
-            imgUrl
+        
+        const { history, setUser } = this.props
+
+        join(this.state)
+            .then(() => logIn(this.state))
+            .then(res => setUser(res.user))
+            .then(() => history.push('/'))
+            .catch(error => {
+                console.error(error)
+                this.setState({
+                    email: '',
+                    password: '',
+                    passwordConfirmation: '',
+                    isError: true,
+                    errorMsg: 'Sign Up Details Invalid'
+                })
+            })
+    }
+    renderError = () => {
+        const toggleForm = this.state.isError ? 'danger' : ''
+        if(this.state.isError) {
+            return (
+            <button type='submit' className={toggleForm}>
+                {this.state.errorMsg}
+            </button>
+            )
+        } else {
+            return <button type='submit'>Sign In</button>
         }
-
-        // console.log({password})
-        // console.log(data)
-        api.post('/users', data)
-            .then((response) => response.status === 201 ? this.props.history.push('/') : null)
-            .catch(() => this.setState({ errorMsg: 'There was an error' }))
     }
-
-    handleChange = e => this.setState({ [e.target.name]: e.target.value })
-
     render() {
-        const { username, firstName, lastName, email, password, imgUrl } = this.state
-        return (
-            <Layout>
-                <h4>Create your account!</h4>
-                <CreateAccForm
-                    formData={{ username, firstName, lastName, email, password, imgUrl }}
-                    onChange={this.handleChange}
-                    onSubmit={this.handleSubmit}
-                />
-            </Layout>
+        const { email, username, password, passwordConfirmation } = this.state
+        
+        return(
+            <div className='row'>
+                <div className='form-container'>
+                    <h3>Sign Up</h3>
+                    <form onSubmit={this.onSignUp}>
+                        <label>Username</label>
+                        <input
+                            required
+                            type='text'
+                            name='username'
+                            value={username}
+                            placeholder='Enter username'
+                            onChange={this.handleChange}
+                        />
+                        <label>Email address</label>
+                        <input
+                            required
+                            type='text'
+                            name='email'
+                            value={email}
+                            placeholder='Enter email'
+                            onChange={this.handleChange}
+                        />
+                        <label>Password</label>
+                        <input
+                            required
+                            type='text'
+                            name='password'
+                            value={password}
+                            placeholder='Enter password'
+                            onChange={this.handleChange}
+                        />
+                        <label>Password Confirmation</label>
+                        <input
+                            required
+                            type='text'
+                            name='passwordConfirmation'
+                            value={passwordConfirmation}
+                            placeholder='Enter passwordConfirmation'
+                            onChange={this.handleChange}
+                        />
+                        {this.renderError()}
+                    </form>
+                </div>
+            </div>
         )
+
     }
+
+
 }
 
-
-export default Join
+export default SignUp
